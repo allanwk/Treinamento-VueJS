b64a78b9a0c5d75827b0db3e458784f96688c629
Allan <allankretzmann@hotmail.com>
2022-08-30 10:44:40 -0300

    Add material do curso token-based-authentication

---
+++ token-based-auth/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1>Welcome to the App!</h1>
+    <div v-if="!loggedIn">
+      To use this app, please
+      <router-link :to="{ name: 'login' }"> Login </router-link>
+      or
+      <router-link :to="{ name: 'register' }"> Register </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { authComputed } from '@/vuex/helpers'
+
+export default {
+  computed: {
+    ...authComputed,
+  },
+}
+</script>
